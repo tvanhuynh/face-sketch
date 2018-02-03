bf999b8d1cd2e4a77a41b0e1f9806ce13b311b4b
tvanhuynh <huynhtu@seas.upenn.edu>
2018-02-03 14:36:48 -0500

    added back/front jaw and restructure draw/render

--- src/components/sketchFace.js
+++ src/components/sketchFace.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 var paper = require('paper');
 
 class SketchFace extends Component {
+
     state = {
         face : {
             // y-coordinates of features
             foreheadLine: .1,
             cheekLine: .4,
-            jawLine: .7,
+            backJawLine: .5,
+            frontJawLine: .7,
             chinLine: .90,
 
             // x-widths of features
             foreheadWidth: .3,
             cheekWidth: .4,
-            jawWidth: .33,
+            backJawWidth: .38,
+            frontJawWidth: .33,
             chinWidth: .1,
         },
         eyes : {
@@ -21,31 +24,56 @@ class SketchFace extends Component {
         },
     };
 
+
     constructor(props) {
         super(props);
+
         this.inputProps = {
             type: 'range',
             step: .01,
             min: 0,
-            max: 1,
+            onChange: this.handleChange,
         }
 
+        this.face = {};
+        this.face.path = new paper.Path();
+        this.face.properties = {
+            forehead: this.calculatePoints(this.state.face.foreheadWidth, this.state.face.foreheadLine),
+            cheek: this.calculatePoints(this.state.face.cheekWidth, this.state.face.cheekLine),
+            backJaw: this.calculatePoints(this.state.face.backJawWidth, this.state.face.backJawLine),
+            frontJaw: this.calculatePoints(this.state.face.frontJawWidth, this.state.face.frontJawLine),
+            chin: this.calculatePoints(this.state.face.chinWidth, this.state.face.chinLine)
+        }
 
-        this.facePath = new paper.Path();
-        this.facePath.strokeColor = 'black';
-        this.facePath.strokeWidth = .5;
+        this.face.order = () => {
+            return(
+                [
+                    this.face.properties.forehead,
+                    this.face.properties.cheek,
+                    this.face.properties.backJaw,
+                    this.face.properties.frontJaw,
+                    this.face.properties.chin
+                ]
+
+            )
+        }
+
+        this.face.path.strokeColor = 'black';
+        this.face.path.strokeWidth = .5;
+
     }
-    
+
 
     componentDidMount() {
+        // Draw Face
         this.drawFace();
     }
+
 
     componentDidUpdate() {
-        this.drawFace();
     }
 
-    
+
     /**
      * Calculate the intersection of two lines on the canvas for both halves.
      * @param {number} x - percentage of whole canvas away from the center
@@ -78,109 +106,136 @@ class SketchFace extends Component {
                 [id]: v
             }
         }));
+
+        switch(id) {
+            case "foreheadLine":
+            case "foreheadWidth":
+                this.face.properties.forehead = this.calculatePoints(this.state.face.foreheadWidth, this.state.face.foreheadLine);
+                this.drawFace();
+                break;
+            case "cheekLine":
+            case "cheekWidth":
+                this.face.properties.cheek = this.calculatePoints(this.state.face.cheekWidth, this.state.face.cheekLine);
+                this.drawFace();
+                break;
+            case "backJawLine":
+            case "backJawWidth":
+                this.face.properties.backJaw = this.calculatePoints(this.state.face.backJawWidth, this.state.face.backJawLine);
+                this.drawFace();
+                break;
+            case "frontJawLine":
+            case "frontJawWidth":
+                this.face.properties.frontJaw = this.calculatePoints(this.state.face.frontJawWidth, this.state.face.frontJawLine);
+                this.drawFace();
+                break;
+            case "chinLine":
+            case "chinWidth":
+                this.face.properties.chin = this.calculatePoints(this.state.face.chinWidth, this.state.face.chinLine);
+                this.drawFace();
+                break;
+            default:
+                // console.log('error');
+        }
      }
 
+     
     /**
      * Draw the shape of the face.
      * @return {null}
      */
     drawFace = () => {
-        this.facePath.removeSegments();
-        
-        // calculate intersections
-        let foreheadIntersection = this.calculatePoints(this.state.face.foreheadWidth, this.state.face.foreheadLine);
-        let cheekIntersection = this.calculatePoints(this.state.face.cheekWidth, this.state.face.cheekLine);
-        let jawIntersection = this.calculatePoints(this.state.face.jawWidth, this.state.face.jawLine);
-        let chinIntersection = this.calculatePoints(this.state.face.chinWidth, this.state.face.chinLine);
+        this.face.path.removeSegments();
 
-        // draw
-        this.facePath.add(foreheadIntersection.left);
-        this.facePath.add(cheekIntersection.left);
-        this.facePath.add(jawIntersection.left);
-        this.facePath.add(chinIntersection.left);
+        const temp = this.face.order();
 
-        this.facePath.add(chinIntersection.right);
-        this.facePath.add(jawIntersection.right);
-        this.facePath.add(cheekIntersection.right);
-        this.facePath.add(foreheadIntersection.right);
+        // draw left side of face
+        temp.forEach(element => {
+            this.face.path.add(element.left);
+        });
 
-        this.facePath.smooth({ type: 'geometric' });
-        // this.facePath.segments[1].handleIn = this.facePath.segments[1].handleIn.normalize(30);
-        // this.facePath.segments[1].handleOut = this.facePath.segments[1].handleOut.normalize(30);
+        // draw right side of face
+        temp.reverse().forEach(element => {
+            this.face.path.add(element.right);
+        });
 
-        this.facePath.fullySelected = true;
+        // add curves
+        this.drawFaceCurves();
+
+        this.face.path.fullySelected = true;
 
         return null;
     }
 
+
+    /**
+     * Adjust the handles of the face
+     * @return {null}
+     */
+
+     drawFaceCurves = () => {
+        this.face.path.smooth({ type: 'geometric' });
+        // this.facePath.segments[1].handleIn = this.facePath.segments[1].handleIn.normalize(300);
+        // this.facePath.segments[1].handleOut = this.facePath.segments[1].handleOut.normalize(300);
+
+        return null;
+     }
+
+
+     /**
+      * Create control for face settings
+      * @param {string} label - What the label of the control should read.
+      * @param {string} stateSetting - Name of the state setting this control is associated with.
+      * @param {number} max - Max number setting for the control
+      * @return {JSX} The rendered controls for setting
+      */
+      
+     makeControl = (label, stateSetting, max = 1) => {
+         return (
+            <div id={stateSetting + '-control'} className="control">
+            <label htmlFor={stateSetting}>{label}</label>
+            <input
+                type='range'
+                step=".01"
+                min="0"
+                max={max}
+                onChange={this.handleChange}
+                id={stateSetting}
+                defaultValue={this.state.face[stateSetting]}
+            />
+            </div>
+         )
+     }
+
+
     render() {
         return (
-
         <div id="face-controls">
+
             <div id="forehead-controls">
-                <label htmlFor="foreheadLine">Forehead Height</label>
-                <input
-                    {...this.inputProps}
-                    id="foreheadLine"
-                    defaultValue={this.state.face.foreheadLine}
-                    onChange={this.handleChange}
-                />
-                <label htmlFor="foreheadWidth">Forehead Width</label>
-                <input
-                    {...this.inputProps}
-                    id="foreheadWidth"
-                    defaultValue={this.state.face.foreheadWidth}
-                    onChange={this.handleChange}
-                />
+                {this.makeControl('Forehead Height', 'foreheadLine')}
+                {this.makeControl('Forehead Width', 'foreheadWidth')}
             </div>
+            
             <div id="cheek-controls">
-                <label htmlFor="cheekLine">Cheek Height</label>
-                <input
-                    {...this.inputProps}
-                    id="cheekLine"
-                    defaultValue={this.state.face.cheekLine}
-                    onChange={this.handleChange}
-                />
-                <label htmlFor="cheekWidth">Cheek Width</label>
-                <input
-                    {...this.inputProps}
-                    id="cheekWidth"
-                    defaultValue={this.state.face.cheekWidth}
-                    onChange={this.handleChange}
-                />
+                {this.makeControl('Cheek Height', 'cheekLine')}
+                {this.makeControl('Cheek Width', 'cheekWidth')}
             </div>
-            <div id="jaw-controls">
-                <label htmlFor="jawLine">Jaw Height</label>
-                <input
-                    {...this.inputProps}
-                    id="jawLine"
-                    defaultValue={this.state.face.jawLine}
-                    onChange={this.handleChange}
-                />
-                <label htmlFor="jawWidth">Jaw Width</label>
-                <input
-                    {...this.inputProps}
-                    id="jawWidth"
-                    defaultValue={this.state.face.jawWidth}
-                    onChange={this.handleChange}
-                />
+            
+            <div id="back-jaw-controls">
+                {this.makeControl('Back Jaw Height', 'backJawLine')}
+                {this.makeControl('Back Jaw Width', 'backJawWidth')}
             </div>
+            
+            <div id="front-jaw-controls">
+                {this.makeControl('Front Jaw Height', 'frontJawLine')}
+                {this.makeControl('Front Jaw Width', 'frontJawWidth')}
+            </div>
+            
             <div id="chin-controls">
-                <label htmlFor="chinLine">Chin Height</label>
-                <input
-                    {...this.inputProps}
-                    id="chinLine"
-                    defaultValue={this.state.face.chinLine}
-                    onChange={this.handleChange}
-                />
-                <label htmlFor="chinWidth">Chin Width</label>
-                <input
-                    {...this.inputProps}
-                    id="chinWidth"
-                    defaultValue={this.state.face.chinWidth}
-                    onChange={this.handleChange}
-                />
+                {this.makeControl('Chin Height', 'chinLine')}
+                {this.makeControl('Chin Width', 'chinWidth')}
             </div>
+
         </div>
         
         );
