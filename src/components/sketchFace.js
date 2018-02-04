6ca2368d8f52c3c4ee79698bc9b416bb5a154472
tvanhuynh <huynhtu@seas.upenn.edu>
2018-02-04 05:35:38 -0500

    lips, jest testing, function clean up

--- src/components/sketchFace.js
+++ src/components/sketchFace.js
@@ -4,29 +4,48 @@ var paper = require('paper');
 class SketchFace extends Component {
 
     state = {
-        face : {
+        autoAdjust: true,
+        face: {
             // y-coordinates of features
-            foreheadLine: .1,
-            cheekLine: .4,
-            cheekConcaveLine: .5,
-            jawLine: .7,
-            chinLine: .90,
+            foreheadLine: .11,
+            cheekLine: .475,
+            cheekConcaveLine: .595,
+            jawLine: .76,
+            chinLine: .935,
 
             // x-widths of features
-            foreheadWidth: .3,
-            cheekWidth: .4,
-            cheekConcaveWidth: .38,
-            jawWidth: .33,
-            chinWidth: .1,
+            foreheadWidth: .225,
+            cheekWidth: .31,
+            cheekConcaveWidth: .295,
+            jawWidth: .255,
+            chinWidth: .08,
 
             // curves of features
-            cheekCurve: .4,
-            cheekConcaveCurve: .38,
-            jawCurve: .33,
-            chinCurve: .1,
+            foreheadCurve: 1,
+            cheekCurve: 1,
+            cheekConcaveCurve: .435,
+            jawCurve: .67,
+            chinCurve: .417,
         },
-        eyes : {
-            eyeHeight: .33,
+        mouth: {
+            // y-coordinates of features
+            lipLine: .76,
+            centerLipLine: .76,
+            lowerLipLine: .8,
+            upperLipLine: .715,
+            cupidsBowLine: .725,
+
+            // x-widths of features
+            lipWidth: .12,
+            lowerLipWidth: .055,
+            upperLipWidth: .045,
+
+            //ruves of features
+            lipCurve: 0,
+            lowerLipCurve: 1,
+            upperLipCurve: .405,
+            cupidsBowCurve: .425,
+
         },
     };
 
@@ -34,13 +53,7 @@ class SketchFace extends Component {
     constructor(props) {
         super(props);
 
-        this.inputProps = {
-            type: 'range',
-            step: .01,
-            min: 0,
-            onChange: this.handleChange,
-        }
-
+        // face
         this.face = {};
         this.face.path = new paper.Path();
         this.face.points = {
@@ -66,43 +79,70 @@ class SketchFace extends Component {
                 curve: () => {return this.state.face.chinCurve}
             }
         }
-
-        this.face.order = ["forehead", "cheek", "cheekConcave", "jaw", "chin"];
-
-        this.face.pointsOrder = () => {
-            return this.face.order.map(i => this.face.points[i].location);
-        }
-
-        this.face.curveOrder = () => {
-            return this.face.order.map(i => this.face.points[i].curve());
-        }
-
-        this.face.path.strokeColor = 'black';
+        this.face.path.strokeColor = 'red';
         this.face.path.strokeWidth = .5;
 
+        // mouth
+        this.mouth = {};
+        this.mouth.path = new paper.Path();
+        this.mouth.centerpath = new paper.Path();
+        this.mouth.points = {
+            lowerLip: {
+                location: this.calculatePoints(this.state.mouth.lowerLipWidth, this.state.mouth.lowerLipLine),
+                curve: () => {return this.state.mouth.lowerLipCurve}
+            },
+            lip: {
+                location: this.calculatePoints(this.state.mouth.lipWidth, this.state.mouth.lipLine),
+                curve: () => {return this.state.mouth.lipCurve}
+            },
+            upperLip: {
+                location: this.calculatePoints(this.state.mouth.upperLipWidth, this.state.mouth.upperLipLine),
+                curve: () => {return this.state.mouth.upperLipCurve}
+            },
+            cupidsBow: {
+                location: this.calculatePoints(0, this.state.mouth.cupidsBowLine),
+                curve: () => {return this.state.mouth.cupidsBowCurve}
+            }
+        }
+        this.mouth.path.strokeColor = 'red';
+        this.mouth.path.strokeWidth = .5;
+        this.mouth.centerpath.strokeColor = 'red';
+        this.mouth.centerpath.strokeWidth = .5;
     }
 
 
     componentDidMount() {
         this.drawFace();
+        this.drawMouth();
     }
 
 
     componentDidUpdate() {
+        console.log(this.state);
     }
 
 
     /**
      * Calculate the intersection of two lines on the canvas for both halves.
-     * @param {number} x - percentage of whole canvas away from the center
-     * @param {number} y - percentage of whole canvas away from the top
+     * @param {number} x - percentage (decimal value) of whole canvas away from the center
+     * @param {number} y - percentage (decimal value) of whole canvas away from the top
      * @return {object} the calculated points, left and right half.
      */
 
      calculatePoints = (x, y) => {
-        const xCoordinateLeft = this.props.canvasWidth / 2 - this.props.canvasWidth * x;
-        const xCoordinateRight = this.props.canvasWidth / 2 + this.props.canvasWidth * x;
-        const yCoordinate = this.props.canvasWidth * y;
+        let canvasWidth = paper.view.size.width;
+
+        if (isNaN(x*1) || isNaN(y*1) || typeof(x) === "boolean" || typeof(y) === "boolean") {
+            throw TypeError;
+        } else if (x < 0 || y < 0 || x > 1 || y > 1) {
+            throw RangeError;
+        } else if (typeof(paper.view.size.width) !== "number" || paper.view.size.width <= 0) {
+            throw Error("Invalid canvas size.");
+        }
+
+        const xCoordinateLeft = canvasWidth / 2 - canvasWidth * x;
+        const xCoordinateRight = canvasWidth / 2 + canvasWidth * x;
+        const yCoordinate = canvasWidth * y;
         return {
             left: new paper.Point(xCoordinateLeft, yCoordinate),
             right: new paper.Point(xCoordinateRight, yCoordinate),
@@ -118,14 +158,16 @@ class SketchFace extends Component {
      handleChange = (event, propChange) => {
         let v = event.target.value;
         let id = event.target.id;
+        let group = event.target.dataset.group;
         this.setState(prevState => ({
-            face: {
-                ...prevState.face,
+            [group]: {
+                ...prevState[group],
                 [id]: v
             }
         }));
 
         switch(id) {
+            // face
             case "foreheadLine":
             case "foreheadWidth":
                 this.face.points.forehead.location = this.calculatePoints(this.state.face.foreheadWidth, this.state.face.foreheadLine);
@@ -158,10 +200,83 @@ class SketchFace extends Component {
             case "chinCurve":
                 this.drawFace();
                 break;
+            // mouth
+            case "lipLine":
+            case "lipWidth":
+                this.mouth.points.lip.location = this.calculatePoints(this.state.mouth.lipWidth, this.state.mouth.lipLine);
+                this.drawMouth();
+                break;
+            case "lowerLipLine":
+            case "lowerLipWidth":
+               this.mouth.points.lowerLip.location = this.calculatePoints(this.state.mouth.lowerLipWidth, this.state.mouth.lowerLipLine);
+               this.drawMouth();
+               break;
+            case "upperLipLine":
+            case "upperLipWidth":
+               this.mouth.points.upperLip.location = this.calculatePoints(this.state.mouth.upperLipWidth, this.state.mouth.upperLipLine);
+               this.drawMouth();
+               break;
+            case "cupidsBowLine":
+                this.mouth.points.cupidsBow.location = this.calculatePoints(0, this.state.mouth.cupidsBowLine);
+                this.drawMouth();
+                break;
+            case "lowerLipCurve":
+            case "upperLipCurve":
+            case "cupidsBowCurve":
+            case "centerLipLine":
+                this.drawMouth();
+                break; 
             default:
                 // console.log('error');
         }
      }
+
+
+    /**
+     * Draw symmetrical
+     * @param {paper.Path} path
+     * @param {array} points
+     * @param {array} curves
+     * @param {boolean} isClosed
+     * @returns {null}
+     */
+
+    drawSymmetric = (path, points, curves, isClosed = true) => {
+        path.removeSegments();
+        isClosed = typeof(isClosed) === "boolean" ? isClosed : true
+
+        // add points
+        points.forEach(element => {
+            const temp = element.left ? element.left : function() {throw TypeError};
+            path.add(temp);
+        });
+
+        points.reverse().forEach(element => {
+            const temp = element.right ? element.right : function() {throw TypeError};
+            path.add(temp);
+        });
+
+        // close path or leave open
+        path.closed = isClosed;
+
+        // adjust curves
+        path.smooth({type:'geometric'});
+        let max = points.length;
+
+        for (let i = 0; i < max; i++) {
+            path.segments[i].handleIn = path.segments[i].handleIn.multiply(curves[i] ? curves[i] : function() {throw TypeError});
+            path.segments[i].handleOut = path.segments[i].handleOut.multiply(curves[i] ? curves[i] : function() {throw TypeError});
+        }
+
+        curves.reverse();
+
+        for (let i = 0; i < max; i++) {
+            path.segments[max + i].handleIn = path.segments[max + i].handleIn.multiply(curves[i] ? curves[i] : function() {throw TypeError});
+            path.segments[max + i].handleOut = path.segments[max + i].handleOut.multiply(curves[i] ? curves[i] : function() {throw TypeError});
+        }
+
+        return null;
+    }
 
      
     /**
@@ -169,30 +284,36 @@ class SketchFace extends Component {
      * @return {null}
      */
     drawFace = () => {
-        this.face.path.removeSegments();
+        const order = ["forehead", "cheek", "cheekConcave", "jaw", "chin"];
+        this.drawSymmetric (
+            this.face.path,
+            order.map(i => this.face.points[i].location),
+            order.map(i => this.face.points[i].curve()),
+        )
+        return null;
+    }
 
-        let temp = this.face.pointsOrder();
+     
+    /**
+     * Draw the shape of the lips.
+     * @return {null}
+     */
+    drawMouth = () => {
+        const order = ["upperLip", "lip", "lowerLip"];
+        this.drawSymmetric (
+            this.mouth.path,
+            order.map(i => this.mouth.points[i].location),
+            order.map(i => this.mouth.points[i].curve()),
+        )
+        this.mouth.path.add(this.mouth.points.cupidsBow.location.left);
+        this.mouth.path.lastSegment.handleIn = new paper.Point(this.state.mouth.upperLipWidth * paper.view.size.width * this.state.mouth.cupidsBowCurve * .5, 0);
+        this.mouth.path.lastSegment.handleOut = new paper.Point(this.state.mouth.upperLipWidth * paper.view.size.width * this.state.mouth.cupidsBowCurve * -.5, 0);
 
-        // draw left side of face
-        temp.forEach(element => {
-            this.face.path.add(element.left);
-        });
-
-        // draw right side of face
-        temp.reverse().forEach(element => {
-            this.face.path.add(element.right);
-        });
-
-        // add curves
-        this.face.path.smooth({ type: 'geometric' });
-        temp = this.face.curveOrder().concat(this.face.curveOrder().reverse());
-        for (let i = 0; i < this.face.path.segments.length; i++) {
-            this.face.path.segments[i].handleIn = this.face.path.segments[i].handleIn.multiply(temp[i]);
-            this.face.path.segments[i].handleOut = this.face.path.segments[i].handleOut.multiply(temp[i]);
-        }
-
-        this.face.path.fullySelected = true;
-
+        this.mouth.centerpath.removeSegments();
+        this.mouth.centerpath.add(this.mouth.points.lip.location.left);
+        this.mouth.centerpath.add(new paper.Point(paper.view.size.width/2, this.state.mouth.centerLipLine * paper.view.size.width));
+        this.mouth.centerpath.add(this.mouth.points.lip.location.right);
+        this.mouth.centerpath.smooth({type:'geometric'});        
         return null;
     }
 
@@ -201,62 +322,85 @@ class SketchFace extends Component {
       * Create control for face settings
       * @param {string} label - What the label of the control should read.
       * @param {string} stateSetting - Name of the state setting this control is associated with.
+      * @param {string} stateGroup - Name of the state group this control is associated with.
       * @param {number} max - Max number setting for the control
       * @return {JSX} The rendered controls for setting
       */
       
-     makeControl = (label, stateSetting, max = 1) => {
+     makeControl = (label, stateSetting, stateGroup, max = 1) => {
+        max = typeof(max) === "number" ? max : 1;
+        if (typeof(label) !== "string" || typeof(stateSetting) !== "string") {
+            throw TypeError;
+        }
+
          return (
             <div id={stateSetting + '-control'} className="control">
-            <label htmlFor={stateSetting}>{label}</label>
-            <input
-                type='range'
-                step=".01"
-                min="0"
-                max={max}
-                onChange={this.handleChange}
-                id={stateSetting}
-                defaultValue={this.state.face[stateSetting]}
-            />
+                <label htmlFor={stateSetting}>{label}</label>
+                <input
+                    type='range'
+                    step=".005"
+                    min="0"
+                    max={max}
+                    onChange={this.handleChange}
+                    id={stateSetting}
+                    defaultValue={this.state[stateGroup][stateSetting]}
+                    data-group={stateGroup}
+                />
             </div>
          )
      }
 
 
     render() {
-        return (
-        <div id="face-controls">
-
-            <div id="forehead-controls">
-                {this.makeControl('Forehead Position', 'foreheadLine')}
-                {this.makeControl('Forehead Width', 'foreheadWidth')}
-                {this.makeControl('Forehead Curve', 'foreheadCurve')}
+        return ( <div id="controls">
+            <div id="face-controls">
+                <div id="forehead-controls">
+                    {this.makeControl('Forehead Position', 'foreheadLine', 'face')}
+                    {this.makeControl('Forehead Width', 'foreheadWidth', 'face')}
+                    {this.makeControl('Forehead Curve', 'foreheadCurve', 'face')}
+                </div>
+                <div id="cheek-controls">
+                    {this.makeControl('Cheek Position', 'cheekLine', 'face')}
+                    {this.makeControl('Cheek Width', 'cheekWidth', 'face')}
+                    {this.makeControl('Cheek Curve', 'cheekCurve', 'face')}
+                    {this.makeControl('Cheek Height', 'cheekConcaveLine', 'face')}
+                    {this.makeControl('Cheek Depth', 'cheekConcaveWidth', 'face')}
+                    {this.makeControl('Cheek Inner Curve', 'cheekConcaveCurve', 'face')}
+                </div>
+                <div id="jaw-controls">
+                    {this.makeControl('Jaw Position', 'jawLine', 'face')}
+                    {this.makeControl('Jaw Width', 'jawWidth', 'face')}
+                    {this.makeControl('Jaw Curve', 'jawCurve', 'face')}
+                </div>
+                <div id="chin-controls">
+                    {this.makeControl('Chin Position', 'chinLine', 'face')}
+                    {this.makeControl('Chin Width', 'chinWidth', 'face')}
+                    {this.makeControl('Chin Curve', 'chinCurve', 'face')}
+                </div>
             </div>
-            
-            <div id="cheek-controls">
-                {this.makeControl('Cheek Position', 'cheekLine')}
-                {this.makeControl('Cheek Width', 'cheekWidth')}
-                {this.makeControl('Cheek Curve', 'cheekCurve')}
-                {this.makeControl('Cheek Height', 'cheekConcaveLine')}
-                {this.makeControl('Cheek Depth', 'cheekConcaveWidth')}
-                {this.makeControl('Cheek Inner Curve', 'cheekConcaveCurve')}
+            <div id="mouth-controls">
+                <div id="lip-controls">
+                    {this.makeControl('Lip Corner Position', 'lipLine', 'mouth')}
+                    {this.makeControl('Lip Center Position', 'centerLipLine', 'mouth')}
+                    {this.makeControl('Lip Width', 'lipWidth', 'mouth')}
+                </div>
+                <div id="lowerLip-controls">
+                    {this.makeControl('Lower Lip Position', 'lowerLipLine', 'mouth')}
+                    {this.makeControl('Lower Lip Width', 'lowerLipWidth', 'mouth')}
+                    {this.makeControl('Lower Lip Curve', 'lowerLipCurve', 'mouth')}
+                </div>
+                <div id="upperLip-controls">
+                    {this.makeControl('Upper Lip Position', 'upperLipLine', 'mouth')}
+                    {this.makeControl('Upper Lip Width', 'upperLipWidth', 'mouth')}
+                    {this.makeControl('Upper Lip Curve', 'upperLipCurve', 'mouth')}
+                </div>
+                <div id="cupidsBow-controls">
+                    {this.makeControl('Cupid\'s Bow Position', 'cupidsBowLine', 'mouth')}
+                    {this.makeControl('Cupid\'s Bow Curve', 'cupidsBowCurve', 'mouth')}
+                </div>
             </div>
-            
-            <div id="jaw-controls">
-                {this.makeControl('Jaw Position', 'jawLine')}
-                {this.makeControl('Jaw Width', 'jawWidth')}
-                {this.makeControl('Jaw Curve', 'jawCurve')}
-            </div>
-            
-            <div id="chin-controls">
-                {this.makeControl('Chin Position', 'chinLine')}
-                {this.makeControl('Chin Width', 'chinWidth')}
-                {this.makeControl('Chin Curve', 'chinCurve')}
-            </div>
-
-        </div>
         
-        );
+        </div>);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import './styles/normalize.css';
 
-import SketchFace from './components/sketchFace';
+import SketchFace from './components/SketchFace';
 
 import registerServiceWorker from './registerServiceWorker';
+
+// import refphoto from './images/reference.png';
 
 var paper = require('paper');
 
 const canvas = document.getElementById('sketchCanvas');
 paper.setup(canvas);
+// canvas.style.background = "url(" + refphoto + ")";
 
-ReactDOM.render(<SketchFace canvasWidth={canvas.clientWidth} />, document.getElementById('controls'));
+ReactDOM.render(<SketchFace />, document.getElementById('controls'));
 
 registerServiceWorker();
